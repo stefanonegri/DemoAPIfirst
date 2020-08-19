495c7ec5fc72ceaad2ad9e617ecb2ea827e85809
stefanonegri <39225773+stefanonegri@users.noreply.github.com>
2020-08-19 17:05:30 +0200

    Create insertAccount.sql

---
+++ insertAccount.sql
@@ -0,0 +1,16 @@
+INSERT INTO 'Accounts'.'Accounts'
+('id',
+'AccountNumber',
+'AccountName',
+'Address',
+'City',
+'State',
+'Consent')
+VALUES
+('0011t000005QvqsAAC',
+'CC978213',
+'GenePoint',
+'345 Shoreline Park',
+'Mountain View',
+'USA',
+'y');
